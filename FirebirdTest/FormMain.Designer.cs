8d09ad33176d8a349c7c2fa8f7f6842fec367514
OctoOsmo <tatarincev_al@bk.ru>
2014-07-18 16:25:25 +0400

    initial commit

---
+++ FirebirdTest/FormMain.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace FirebirdTest
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.textBoxDBPath = new System.Windows.Forms.TextBox();
+            this.textBoxLogin = new System.Windows.Forms.TextBox();
+            this.textBoxPass = new System.Windows.Forms.TextBox();
+            this.buttonSelect = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(209, 51);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 0;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(442, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выходToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(108, 22);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // textBoxDBPath
+            // 
+            this.textBoxDBPath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDBPath.Location = new System.Drawing.Point(12, 27);
+            this.textBoxDBPath.Name = "textBoxDBPath";
+            this.textBoxDBPath.Size = new System.Drawing.Size(420, 20);
+            this.textBoxDBPath.TabIndex = 2;
+            this.textBoxDBPath.Text = "D:\\Программы\\Тесты\\Последний релиз\\TEST_VSMA.GDB";
+            // 
+            // textBoxLogin
+            // 
+            this.textBoxLogin.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.textBoxLogin.Location = new System.Drawing.Point(12, 53);
+            this.textBoxLogin.Name = "textBoxLogin";
+            this.textBoxLogin.Size = new System.Drawing.Size(191, 20);
+            this.textBoxLogin.TabIndex = 3;
+            this.textBoxLogin.Text = "SYSDBA";
+            // 
+            // textBoxPass
+            // 
+            this.textBoxPass.Location = new System.Drawing.Point(12, 79);
+            this.textBoxPass.Name = "textBoxPass";
+            this.textBoxPass.Size = new System.Drawing.Size(191, 20);
+            this.textBoxPass.TabIndex = 4;
+            this.textBoxPass.Text = "masterkey";
+            // 
+            // buttonSelect
+            // 
+            this.buttonSelect.Location = new System.Drawing.Point(209, 77);
+            this.buttonSelect.Name = "buttonSelect";
+            this.buttonSelect.Size = new System.Drawing.Size(75, 23);
+            this.buttonSelect.TabIndex = 5;
+            this.buttonSelect.Text = "Select";
+            this.buttonSelect.UseVisualStyleBackColor = true;
+            this.buttonSelect.Click += new System.EventHandler(this.buttonSelect_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(442, 315);
+            this.Controls.Add(this.buttonSelect);
+            this.Controls.Add(this.textBoxPass);
+            this.Controls.Add(this.textBoxLogin);
+            this.Controls.Add(this.textBoxDBPath);
+            this.Controls.Add(this.buttonConnect);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormMain";
+            this.Text = "Firebird Test";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.TextBox textBoxDBPath;
+        private System.Windows.Forms.TextBox textBoxLogin;
+        private System.Windows.Forms.TextBox textBoxPass;
+        private System.Windows.Forms.Button buttonSelect;
+    }
+}
+
---
+++ FirebirdTest/FormMain.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using FirebirdSql.Data.FirebirdClient;
+//using FirebirdSql.Data.Common;
+
+namespace FirebirdTest
+{
+    public partial class FormMain : Form
+    {
+        //FirebirdSql.Data.FirebirdClient.
+        FbConnection fb;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            //формируем connection string для последующего соединения с нашей базой данных
+            FbConnectionStringBuilder fb_con = new FbConnectionStringBuilder();
+            fb_con.Charset = "WIN1251"; //используемая кодировка
+            fb_con.UserID = textBoxLogin.Text; //логин
+            fb_con.Password = textBoxPass.Text; //пароль
+            fb_con.Database = textBoxDBPath.Text; //путь к файлу базы данных
+            fb_con.ServerType = 0; //указываем тип сервера (0 - "полноценный Firebird" (classic или super server), 1 - встроенный (embedded))
+
+            //создаем подключение
+            fb = new FbConnection(fb_con.ToString()); //передаем нашу строку подключения объекту класса FbConnection
+
+            fb.Open(); //открываем БД
+
+            FbDatabaseInfo fb_inf = new FbDatabaseInfo(fb); //информация о БД
+
+            //пока у объекта БД не был вызван метод Open() - никакой информации о БД не получить, будет только ошибка
+            MessageBox.Show("Info: " + fb_inf.ServerClass + "; " + fb_inf.ServerVersion); //выводим тип и версию используемого сервера Firebird
+        }
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void buttonSelect_Click(object sender, EventArgs e)
+        {
+            //так проверять состояние соединения (активно или не активно)
+            if (fb.State == ConnectionState.Closed)
+                fb.Open();
+
+
+            FbTransaction fbt = fb.BeginTransaction(); //стартуем транзакцию; стартовать транзакцию можно только для открытой базы (т.е. мутод Open() уже был вызван ранее, иначе ошибка)
+
+
+            FbCommand SelectSQL = new FbCommand("SELECT * FROM SPISOK_ST", fb); //задаем запрос на выборку
+
+
+            SelectSQL.Transaction = fbt; //необходимо проинициализить транзакцию для объекта SelectSQL
+            FbDataReader reader = SelectSQL.ExecuteReader(); //для запросов, которые возвращают результат в виде набора данных надо использоваться метод ExecuteReader()
+
+
+            string select_result = ""; //в эту переменную будем складывать результат запроса Select
+            //textBoxResult.Clear();
+
+
+            try
+            {
+                while (reader.Read()) //пока не прочли все данные выполняем...
+                {
+                    // select_result = select_result + reader.GetInt32(0).ToString() + ", " + reader.GetString(1) + "\n";
+                    select_result = select_result + reader.GetString(0) + " " + reader.GetString(1) + " " + reader.GetString(2) + " " + reader.GetString(3) + "\n";
+                    //textBoxResult.Text += reader.GetString(0) + " " + reader.GetString(1) + " " + reader.GetString(2) + " " + reader.GetString(3) + "\n";
+                }
+            }
+            finally
+            {
+                //всегда необходимо вызывать метод Close(), когда чтение данных завершено
+                reader.Close();
+                fb.Close(); //закрываем соединение, т.к. оно нам больше не нужно
+            }
+            MessageBox.Show(select_result); //выводим результат запроса
+            SelectSQL.Dispose(); //в документации написано, что ОЧЕНЬ рекомендуется убивать объекты этого типа, если они больше не нужны
+        }
+    }
+}
